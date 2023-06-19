598f9aa43000ac2d577714a12b7e25f337948d11
dojo2023-suzukimoeka <suzukimoekaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 17:21:07 +0900

    Create LogindataDAO.java

---
+++ src/src/dao/LogindataDAO.java
@@ -0,0 +1,64 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Logindata;
+
+public class LogindataDAO {
+	// ログインできるならtrueを返す
+	public boolean isLoginOK(Logindata idpwre) {
+		Connection conn = null;
+		boolean loginResult = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6Data/B4", "sa", "");
+
+			// SELECT文を準備する
+			String sql = "select count(*) from IDPW where ID = ? and PW = ? and RE =?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, idpwre.getId());
+			pStmt.setString(2,idpwre.getPw());
+			pStmt.setString(3,idpwre.getRe() );
+
+			// SELECT文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// ユーザーIDとパスワードが一致するユーザーがいたかどうかをチェックする
+			rs.next();
+			if (rs.getInt("count(*)") == 1) {
+				loginResult = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			loginResult = false;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					loginResult = false;
+				}
+			}
+		}
+
+		// 結果を返す
+		return loginResult;
+	}
+}
