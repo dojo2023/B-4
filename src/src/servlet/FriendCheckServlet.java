27eb344eda021aa51da58309618ef8490ae061d4
dojo2023-fujitar <130331228+dojo2023-fujitar@users.noreply.github.com>
2023-06-23 13:46:26 +0900

    6/23

---
+++ src/src/servlet/FriendCheckServlet.java
@@ -0,0 +1,72 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.UsersDAO;
+
+/**
+ * Servlet implementation class FriendCheckServlet
+ */
+@WebServlet("/FriendCheckServlet")
+public class FriendCheckServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FriendCheckServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("hello");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/**protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}*/
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		request.setCharacterEncoding("UTF-8");
+
+		//idを受け取る
+		String id = request.getParameter("search");
+		//idで検索する
+		UsersDAO uDao = new UsersDAO();
+
+
+
+
+		//結果を返す
+		if(uDao.setUser_id(id)) {
+			//いた
+			response.getWriter().append("true");
+		}else {
+			//いない
+			response.getWriter().append("false");
+		}
+
+
+	}
+}
+
